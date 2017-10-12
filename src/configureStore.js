c8daf130b31c61d729095a5928ccfd06758be5cd
er3n <erenozturk@gmail.com>
2017-10-12 23:44:51 +0300

    devtools eklendi

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import app from './reducers'
 import promiseMiddleware from 'redux-promise-middleware';
+import { composeWithDevTools } from 'remote-redux-devtools';
 
 export default function configureStore() {
-    let store = createStore(app, applyMiddleware(promiseMiddleware()))
+    let store = createStore(app, composeWithDevTools(applyMiddleware(promiseMiddleware())))
     return store
 }
